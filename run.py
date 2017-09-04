2a09dda7f9602e65f63a09c6a855c185b59707cf
Ondrej Luks <lukso@e-btlnet.local>
2017-09-04 15:54:22 +0200

    py 2->3

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
+    print('Starting httpd...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
