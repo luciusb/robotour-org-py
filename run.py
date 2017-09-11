25e102e66361577a028824d14679c6e69503655e
Ondrej Luks <lukso@e-btlnet.local>
2017-09-11 07:46:14 +0200

    i

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
 import pyqrcode
+from StringIO import StringIO
 import sys
+from time import time as now
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-class S(BaseHTTPRequestHandler):
+
+
+class myRequestHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_GET(self):
+
+        if not self.server._svg:
+            svg = StringIO()
+            qr = pyqrcode.create('robotour')
+            qr.svg(svg, scale=10)
+            self.server._svg = svg.getvalue()
+        response = "<html><body><h1>Robotour 2017</h1>\n%s\n</body></html>"%self.server._svg
         self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        self.wfile.write(response)
 
     def do_HEAD(self):
         self._set_headers()
@@ -21,9 +32,16 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
         self.wfile.write("<html><body><h1>POST!</h1></body></html>")
 
-def run(server_class=HTTPServer, handler_class=S, port=80):
+
+def readconfig(fn):
+    with open(fn,'r') as f:
+        table = (eval(line) for line in f)
+    return table
+
+def run(port=80):
     server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+    httpd = HTTPServer(server_address, myRequestHandler)
+    httpd._svg=None
     print('Starting httpd...')
     httpd.serve_forever()
 
@@ -34,6 +52,3 @@ if __name__ == "__main__":
         run(port=int(argv[1]))
     else:
         run(port=8888)
-
-
-#url.svg('robotour.svg', scale=4)
