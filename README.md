d72f5d2eef010479d66c055f72dfdb90df946574
lukso <ondrej.luks@medictech.com>
2018-06-20 12:28:21 +0200

    cleaunp

--- README.md
+++
@@ -1,2 +0,0 @@
-Python script that shall serve as Goal serving server for Robotour 2017.
-Should run on Android tablet.
--- upload.py
+++
@@ -1,6 +0,0 @@
-from requests import post
-uri='http://localhost:8888/results'
-uri='http://192.168.43.1:8888/results'
-h = post(uri, data=open('results.txt','rb'))
-print(h)
-
